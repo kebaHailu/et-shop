07e6ac6872e32222c64b6ca5973301b28e880a4b
kebaHailu <kibromhailu.5513@gmail.com>
2025-05-19 16:14:50 +0300

    fix responsive design action

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
 import Logo from "../assets/images/logo.png";
 import { Link, NavLink } from "react-router-dom";
 import { useCart } from "../context/CartContext";
+import { useState } from "react";
 
 export const Header = () => {
   const { cart } = useCart();
   const notificationCount = cart.length;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   // Define active and inactive classes
   const activeClass =
     "block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0";
   const inactiveClass =
     "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0";
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <nav className="bg-white border-gray-200">
@@ -22,13 +28,13 @@ export const Header = () => {
           </span>
         </a>
         <button
-          data-collapse-toggle="navbar-default"
+          onClick={toggleMenu}
           type="button"
           className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
           aria-controls="navbar-default"
-          aria-expanded="false"
+          aria-expanded={isMenuOpen}
         >
-          <span className="sr-only">Open main menu</span>
+          <span className="sr-only">Toggle menu</span>
           <svg
             className="w-5 h-5"
             aria-hidden="true"
@@ -45,7 +51,12 @@ export const Header = () => {
             />
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } w-full md:block md:w-auto`}
+          id="navbar-default"
+        >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
             <li>
               <NavLink
@@ -53,6 +64,7 @@ export const Header = () => {
                 className={({ isActive }) =>
                   isActive ? activeClass : inactiveClass
                 }
+                onClick={() => setIsMenuOpen(false)}
                 aria-current="page"
               >
                 Home
@@ -66,6 +78,7 @@ export const Header = () => {
                   className={({ isActive }) =>
                     isActive ? activeClass : inactiveClass
                   }
+                  onClick={() => setIsMenuOpen(false)}
                 >
                   My cart
                 </NavLink>
